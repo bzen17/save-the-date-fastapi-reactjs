48a848a61868acf5353d4296eef94e7c89674f09
Ayan Banerjee <ayanbanerjee95@outlook.com>
2022-04-28 00:26:47 +0530

    Init

--- server/config/importEnv.py
+++ server/config/importEnv.py
@@ -1,7 +1,6 @@
 from dotenv import load_dotenv
 import os
-load_dotenv() 
-
+load_dotenv() # Loads the .env file
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
